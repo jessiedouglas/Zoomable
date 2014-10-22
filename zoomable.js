4acc3e6c2eff381e953367e81767f25f11fff599
App Academy Student <student@appacademy.io>
2014-10-22 19:14:55 -0400

    Add styling, fix zoomed image, make work on any width page

--- zoomable.js
+++ zoomable.js
@@ -2,8 +2,20 @@ $.Zoomable = function (el) {
   this.$el = $(el);
   this.sizeFocusBox = 50
 
+  this.setCss()
+
   $(".zoomable").on("mousemove", this.showFocusBox.bind(this));
   $(".zoomable").on("mouseleave", this.removeFocusBox.bind(this));
+};
+
+// SET IN CSS; DO NOT CHANGE WITHOUT CHANGING CSS
+  $.Zoomable.PAGELEFT = 230;
+  $.Zoomable.PAGETOP = 135;
+  $.Zoomable.imageSize = 200;
+  $.Zoomable.borderSize = 5;
+
+$.Zoomable.prototype.setCss = function () {
+  $(".zoomable").css("width", $.Zoomable.imageSize + "px")
 };
 
 $.Zoomable.prototype.showFocusBox = function (event) {
@@ -12,28 +24,27 @@ $.Zoomable.prototype.showFocusBox = function (event) {
   }
 
   this.$focusBox = this.$focusBox || $(".focus-box");
-  var imageWidth = 200;
-  var imageHeight = 200;
-  if (event.pageX - this.sizeFocusBox / 2 < 0) {
+
+  if (event.pageX - this.sizeFocusBox < $.Zoomable.PAGELEFT + $.Zoomable.borderSize) {
     var boxLeft = 0;
-  } else if (event.pageX + this.sizeFocusBox / 2 > imageWidth) {
-    var boxLeft = imageWidth - this.sizeFocusBox;
+  } else if (event.pageX > $.Zoomable.PAGELEFT + $.Zoomable.imageSize) {
+    var boxLeft = $.Zoomable.imageSize - this.sizeFocusBox;
   } else {
-    var boxLeft = event.pageX - 25;
+    var boxLeft = event.pageX - this.sizeFocusBox - $.Zoomable.PAGELEFT - $.Zoomable.borderSize;
   }
 
-  if (event.pageY - this.sizeFocusBox / 2 < 0) {
+  if (event.pageY - this.sizeFocusBox / 2 < $.Zoomable.PAGETOP + 0) {
     var boxTop = 0;
-  } else if (event.pageY + this.sizeFocusBox / 2 > imageHeight) {
-    var boxTop = imageHeight - this.sizeFocusBox;
+  } else if (event.pageY > $.Zoomable.PAGETOP + $.Zoomable.imageSize - this.sizeFocusBox / 2 ) {
+    var boxTop = $.Zoomable.imageSize - this.sizeFocusBox;
   } else {
-    var boxTop = event.pageY - 25;
+    var boxTop = event.pageY - this.sizeFocusBox / 2 - $.Zoomable.PAGETOP;
   }
 
   $(".focus-box").css("top", boxTop);
   $(".focus-box").css("left", boxLeft);
 
-  this.showZoom(boxTop + 25, boxLeft + 25);
+  this.showZoom(boxLeft, boxTop);
 };
 
 $.Zoomable.prototype.removeFocusBox = function (event) {
@@ -45,18 +56,20 @@ $.Zoomable.prototype.removeFocusBox = function (event) {
 };
 
 $.Zoomable.prototype.showZoom = function (xDiff, yDiff) {
-  var scale = (200 / this.sizeFocusBox) * 100
-  var backgroundPosition =  (yDiff/2) + "%" + (xDiff/2) + "%";
+  var scale = ($.Zoomable.imageSize / this.sizeFocusBox) * 100
+  var heightDiff = $.Zoomable.imageSize - this.sizeFocusBox
+  var backgroundPosition =  (xDiff * 100)/heightDiff + "% " + (yDiff*100)/heightDiff + "%";
 
 
   if (!this.$zoomedImage) {
     $("body").append('<div class="zoomed-image">');
   }
   var backgroundImage = 'url(' + $(".zoomable > img").attr("src") + ')'
+  var height = $(window).height() > 600 ? 600 : $(window).height()
 
   this.$zoomedImage = this.$zoomedImage || $(".zoomed-image")
-  $(".zoomed-image").css("width", $(window).height());
-  $(".zoomed-image").css("height", $(window).height());
+  $(".zoomed-image").css("width", height);
+  $(".zoomed-image").css("height", height);
   $(".zoomed-image").css("background-image", backgroundImage);
   $(".zoomed-image").css("background-size", scale + "%");
   $(".zoomed-image").css("background-position", backgroundPosition)
